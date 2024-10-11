b8419163ca51a319b724962102d770b9358096a7
g.sartori <g.sartori@gmail.com>
2024-10-11 17:35:18 +0200

    imp: SQLUtils cleanups

--- commons-utils/src/main/groovy/dueuno/commons/utils/SqlUtils.groovy
+++ commons-utils/src/main/groovy/dueuno/commons/utils/SqlUtils.groovy
@@ -106,12 +106,12 @@ class SqlUtils {
         return result
     }
 
-    static List<List<Object>> executeInsert(DataSource dataSource, String query) {
+    static List<GroovyRowResult> executeSelect(DataSource dataSource, String query) {
         Sql sql = new Sql(dataSource)
         log.trace(query)
-        List<List<Object>> result = sql.executeInsert(query)
+        List<GroovyRowResult> results = sql.rows(query)
         sql.close()
-        return result
+        return results
     }
 
     private static String generateWhere(Map<String, Object> filterParams, List<String> fieldsInLike = []) {
