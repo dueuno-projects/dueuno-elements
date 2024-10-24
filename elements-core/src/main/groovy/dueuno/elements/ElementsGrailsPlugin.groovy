3d5e60894d887894a8b60fb01dcedce70a4c6d9d
g.sartori <g.sartori@gmail.com>
2024-10-24 15:08:49 +0200

    Revert "imp: Dependencies update & cleanups"
    This reverts commit c6b970b8ed3bf042ed840950a7df2a42436f5c7f.

--- elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
+++ elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
@@ -29,9 +29,9 @@ class ElementsGrailsPlugin extends Plugin {
 
     static final String NAME = 'elements-core'
 
-    def version = '2.0-SNAPSHOT'
+    def version = '2.0.0'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = '6.2.1 > *'
+    def grailsVersion = '6.2.0 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         'grails-app/views/error.gsp'
