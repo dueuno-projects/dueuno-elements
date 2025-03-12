09ca0b139b9b5c0a6336c91d65bc490e6b0b6376
piceghello <fpiceghello@gmail.com>
2025-03-12 17:28:08 +0100

    wip

--- app-test/grails-app/assets/dueuno/elements/components/KeyPress.js
+++ app-test/grails-app/assets/dueuno/elements/components/KeyPress.js
@@ -2,8 +2,8 @@ class KeyPress extends Component {
 
      static finalize($element, $root) {
          $(document)
-            .off('keypress.keyPress')
-            .on('keypress.keyPress', '*', KeyPress.onKeyPress);
+            .off('keydown.keyPress')
+            .on('keydown.keyPress', KeyPress.onKeyPress);
      }
 
      static onKeyPress(event) {
@@ -17,16 +17,46 @@ class KeyPress extends Component {
             return;
         }
 
-        if (event.key != 'Enter' || triggerKey.length == 0) {
+        if (isNaN(this.timer)) this.timer = 0;
+        let t = Date.now();
+        let timeInterval = t - this.timer;
+        this.timer = t;
+
+        //a barcode reader is typically much faster at typing than a human...
+        //we use this principle to understand if the typing comes from a reader
+        let checkMinTime = (timeInterval < 15);
+
+        //to prevent accidental typing, the buffer empties after a certain time
+        let checkMaxTime = (timeInterval > 1500);
+        if (checkMaxTime) {
+            $search.val('');
+        }
+
+        let keycode = event.keyCode;
+        let valid =
+           (keycode > 47 && keycode < 58)   || // number keys
+           (keycode == 32)                  || // space bar
+           (keycode > 64 && keycode < 91)   || // letter keys
+           (keycode > 95 && keycode < 112)  || // numpad keys
+           (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+           (keycode > 218 && keycode < 223);   // [\]' (in order)
+
+        if (valid) {
             $search.val($search.val() + event.key);
         }
 
         if (event.key == triggerKey || triggerKey.length == 0) {
-            let triggerEvent = Component.getEvent($element, 'keypress');
-            if (triggerEvent) {
-                if (!triggerEvent.params) triggerEvent.params = {};
-                triggerEvent.params['_21KeyPressed'] = $search.val();
-                Transition.submit(triggerEvent);
+            if ($search.val().length > 0) {
+                if (event.target.tagName == 'A') {
+                    event.preventDefault();
+                }
+
+                let triggerEvent = Component.getEvent($element, 'keypress');
+                if (triggerEvent && (event.target.tagName != 'INPUT' || checkMinTime)) {
+                    if (!triggerEvent.params) triggerEvent.params = {};
+                    triggerEvent.params['_21KeyPressed'] = $search.val();
+                    Transition.submit(triggerEvent);
+                }
                 $search.val('');
             }
         }
