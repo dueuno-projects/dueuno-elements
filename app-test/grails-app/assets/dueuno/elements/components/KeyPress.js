01313071cffce9b7d5cfb96bc6762f71858d8dab
piceghello <fpiceghello@gmail.com>
2025-03-19 17:39:42 +0100

    cleanups

--- app-test/grails-app/assets/dueuno/elements/components/KeyPress.js
+++ app-test/grails-app/assets/dueuno/elements/components/KeyPress.js
@@ -1,12 +1,12 @@
 class KeyPress extends Component {
 
-     static finalize($element, $root) {
-         $(document)
+    static finalize($element, $root) {
+        $(document)
             .off('keydown.keyPress')
             .on('keydown.keyPress', KeyPress.onKeyPress);
-     }
+    }
 
-     static onKeyPress(event) {
+    static onKeyPress(event) {
         let $element = $('[data-21-component="KeyPress"]');
         let $search = $element.find('input');
 
@@ -15,6 +15,7 @@ class KeyPress extends Component {
         let triggerKey = Component.getProperty($element, 'triggerKey');
         let readingSpeed = Component.getProperty($element, 'readingSpeed');
         let bufferCleanupTimeout = Component.getProperty($element, 'bufferCleanupTimeout');
+        let keepClean = Component.getProperty($element, 'keepClean');
         let timer = Component.getProperty($element, 'timer');
 
         if (triggerKey == null) {
@@ -28,39 +29,48 @@ class KeyPress extends Component {
 
         let checkMinTime = (readingSpeed == 0 || timeInterval < readingSpeed);
         let checkMaxTime = (bufferCleanupTimeout == 0 || timeInterval > bufferCleanupTimeout);
+
         if (checkMaxTime) {
             $search.val('');
         }
 
         let keycode = event.keyCode;
-        let valid =
-           (keycode > 47 && keycode < 58)   || // number keys
-           (keycode == 32)                  || // space bar
-           (keycode > 64 && keycode < 91)   || // letter keys
-           (keycode > 95 && keycode < 112)  || // numpad keys
-           (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-           (keycode > 218 && keycode < 223);   // [\]' (in order)
+        let printable =
+            (keycode > 47 && keycode < 58)   || // number keys
+            (keycode == 32)                  || // space bar
+            (keycode > 64 && keycode < 91)   || // letter keys
+            (keycode > 95 && keycode < 112)  || // numpad keys
+            (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+            (keycode > 218 && keycode < 223);   // [\]' (in order)
 
-        if (valid) {
-            $search.val($search.val() + event.key);
-        }
-
-        if (event.key == triggerKey || triggerKey.length == 0) {
-            if ($search.val().length > 0) {
-                if (event.target.tagName == 'A') {
-                    event.preventDefault();
-                }
-
-                let triggerEvent = Component.getEvent($element, 'keypress');
-                if (triggerEvent && (event.target.tagName != 'INPUT' || checkMinTime)) {
-                    if (!triggerEvent.params) triggerEvent.params = {};
-                    triggerEvent.params['_21KeyPressed'] = $search.val();
-                    Transition.submit(triggerEvent);
-                }
-                $search.val('');
+        if (triggerKey.length == 0) {
+            $search.val(event.key);
+        } else {
+            if (printable) {
+                $search.val($search.val() + event.key);
             }
         }
-     }
+
+        if (keepClean && !event.ctrlKey && checkMinTime && event.target.tagName == 'INPUT') {
+            event.preventDefault();
+            $(event.target).val('');
+        }
+
+        if ((event.key == triggerKey && $search.val().length > 0) || triggerKey.length == 0) {
+            if (triggerKey.length > 0 && $search.val().length > 0 &&
+                (event.target.tagName == 'A' || event.target.tagName == 'BUTTON')) {
+                event.preventDefault();
+            }
+
+            let triggerEvent = Component.getEvent($element, 'keypress');
+            if (triggerEvent && (event.target.tagName != 'INPUT' || !printable || checkMinTime)) {
+                if (!triggerEvent.params) triggerEvent.params = {};
+                triggerEvent.params['_21KeyPressed'] = $search.val();
+                Transition.submit(triggerEvent);
+            }
+            $search.val('');
+        }
+    }
 }
 
 Component.register(KeyPress);
