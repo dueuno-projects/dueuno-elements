76bc87533ee1058d858a5a0db89406dd78304933
piceghello <fpiceghello@gmail.com>
2025-03-25 17:54:44 +0100

    imp: cleanups

--- elements-core/grails-app/assets/dueuno/elements/pages/Login.js
+++ elements-core/grails-app/assets/dueuno/elements/pages/Login.js
@@ -12,7 +12,7 @@ class Login extends Page {
 
         event.stopPropagation();
 
-        let readingSpeed = 40;
+        let readingSpeed = 50;
         let bufferCleanupTimeout = 500;
         let timer = Component.getProperty($element, 'timer');
 
