5395967bcf720d51725778b37ed3a5010502638e
g.sartori <g.sartori@gmail.com>
2025-11-04 17:46:10 +0100

    2.9.4

--- elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
+++ elements-core/src/main/groovy/dueuno/elements/ElementsGrailsPlugin.groovy
@@ -34,7 +34,7 @@ class ElementsGrailsPlugin extends Plugin {
 
     static final String NAME = 'elements-core'
 
-    def version = '2.9.3'
+    def version = '2.9.4'
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = '6.2.0 > *'
     // resources that are excluded from plugin packaging
