b7b32407ab2eeab3854ce7a03e2405b313e9853a
g.sartori <g.sartori@gmail.com>
2026-04-09 10:03:15 +0200

    ErrorCode -> ValidationError

--- dueuno-core/src/main/groovy/dueuno/validation/ValidationError.groovy
+++ dueuno-core/src/main/groovy/dueuno/validation/ValidationError.groovy
@@ -3,7 +3,7 @@ package dueuno.validation
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class ErrorCode {
+class ValidationError {
 
     static final String IS_REQUIRED = 'nullable'
     static final String IS_NOT_UNIQUE = 'unique'
